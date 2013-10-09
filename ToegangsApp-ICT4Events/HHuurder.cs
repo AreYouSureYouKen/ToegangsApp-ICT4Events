574aa75a0a6993944955e57ac515c52386b3e311
blazinsoul <lukevdloop@hotmail.com>
2013-10-09 11:23:36 +0200

    Classes Aangemaakt

---
+++ ToegangsApp-ICT4Events/HHuurder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToegangsApp_ICT4Events
+{
+    class HHuurder : Huurder
+    {
+        private string emailadres;
+        private string rekeningnummer;
+
+        public HHuurder(string Emailadres, string Rekeningnummer, bool Aanwezig, string Adres, string Documentnr, string Geboortedatum, string Naam, int RFIDCode, string Woonplaats)
+            : base(Aanwezig,Adres,Documentnr,Geboortedatum,Naam,RFIDCode,Woonplaats)
+        {
+            emailadres = Emailadres;
+            rekeningnummer = Rekeningnummer;
+        }
+    }
+}
---
+++ ToegangsApp-ICT4Events/Huurder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToegangsApp_ICT4Events
+{
+    class Huurder
+    {
+        protected bool aanwezig;
+        protected string adres;
+        protected string documentnr;
+        protected string geboortedatum;
+        protected string naam;
+        protected int RFIDcode;
+        protected string woonplaats;
+
+        public Huurder(bool Aanwezig, string Adres, string Documentnr, string Geboortedatum, string Naam, int RFIDCode, string Woonplaats)
+        {
+            aanwezig = Aanwezig;
+            adres = Adres;
+            documentnr = Documentnr;
+            geboortedatum = Geboortedatum;
+            naam = Naam;
+            RFIDcode = RFIDCode;
+            woonplaats = Woonplaats;
+        }
+    }
+}
